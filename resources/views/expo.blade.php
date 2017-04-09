d5288f645a438fb4beaeda7ebf5d92bf9d0fd560
cheyuwu <frank890417@gmail.com>
2017-04-09 23:41:56 +0800

    update news page

--- resources/views/expo.blade.php
+++ resources/views/expo.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('content')
-  <div class='container'>
+  <div class='container page_expo'>
     <div class='row'>
 
       <div class="col-sm-6">
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -9,34 +9,34 @@
 
 @section('content')
 
-<div class='container-fluid' style='padding: 0'>
+<div class='container-fluid page_news'>
 
-	<div style="height: 200px;background-image: url(http://zashare.org/img/ci_people.png);background-color: #f24;background-repeat: no-repeat;background-position: 30% 20%"></div>
-	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<br>
-				<h1>【有看見去年的你嗎？】2016雜學校回顧影片釋出！</h1>
-				<hr>	
-			</div>
-			<div class='col-sm-12'>
-				<iframe width="100%" height="500" src="https://www.youtube.com/embed/fBgHQJG8UOM" frameborder="0" allowfullscreen></iframe>
-				<br><br>
-				<h4>
-					匯聚所有學校沒教<br><br>
-					適合每一個你的不同學習路徑<br>
-				</h4>
-				<p>
-					
+	<div class="red_bg">
+		<div class="container area_news">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>【有看見去年的你嗎？】2016雜學校回顧影片釋出！</h1>
 					<br>
-					雜學校，亞洲最大創新教育展（前身為2015不太乖教育節）。通過針對創新教育單位的邀展，結合論壇、實作工作坊與主題策展，打造全新教育策展體驗。去年的你，到現場「雜」了嗎？！快來看看影片找尋你的身影吧！（錯過了？沒關係！看完影片一樣讓你身歷其境～！）<br><br>
-					因為有了每一個你的大力相挺，2017雜學校正籌備著一樁前所未見的擴校計畫！從包下”整個華山“，然後再包下“整座城市”，雜學校希望學習這件事像空氣一樣充滿在每個角落～更落實讓1-99歲的所有人，都「學你想學，學你想成為」！<br>	<br>
-					打勾勾，<br>
-					今年我們相約10月20-22日 華山1914文化創意產業園區 見！
-				</p>
-			</div>
-		</div>		
+					<br>
+				</div>
+				<div class='col-sm-12'>
+					<iframe width="100%" height="500" src="https://www.youtube.com/embed/fBgHQJG8UOM" frameborder="0" allowfullscreen></iframe>
+					<br><br>
+					<h4>
+						匯聚所有學校沒教<br><br>
+						適合每一個你的不同學習路徑<br>
+					</h4>
+					<p>
+						
+						<br>
+						雜學校，亞洲最大創新教育展（前身為2015不太乖教育節）。通過針對創新教育單位的邀展，結合論壇、實作工作坊與主題策展，打造全新教育策展體驗。去年的你，到現場「雜」了嗎？！快來看看影片找尋你的身影吧！（錯過了？沒關係！看完影片一樣讓你身歷其境～！）<br><br>
+						因為有了每一個你的大力相挺，2017雜學校正籌備著一樁前所未見的擴校計畫！從包下”整個華山“，然後再包下“整座城市”，雜學校希望學習這件事像空氣一樣充滿在每個角落～更落實讓1-99歲的所有人，都「學你想學，學你想成為」！<br>	<br>
+						打勾勾，<br>
+						今年我們相約10月20-22日 華山1914文化創意產業園區 見！
+					</p>
+				</div>
+			</div>		
+		</div>
 	</div>
 	
 
