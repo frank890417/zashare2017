e53e93ead7b80df7f6f599f74eaf4b21842cb1e1
cheyuwu <frank890417@gmail.com>
2017-08-18 17:21:45 +0800

    update pacman part

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -91,4 +91,11 @@ class HomeController extends Controller
             ->with('pagename','fund')
             ->with('title','募資頁面');
     }
+
+
+    public function packman(){
+      return view('pacman')
+            ->with('pagename','fund')
+            ->with('title','募資頁面');
+    }
 }
