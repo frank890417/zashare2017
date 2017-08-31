99b24f3a0628bd7c9079cfaa4a842e59859cf2d1
cheyuwu <frank890417@gmail.com>
2017-09-01 02:46:45 +0800

    update news 2

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -11,6 +11,56 @@
 @section('content')
 
 <div class='container-fluid page_news'>
+	<div class="orange_bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-11">
+					<h1 class="newsTitle">雜學校募資大業啟動 擔任建校元老就趁現在!</h1>
+					<br>
+					<p>在這裡,教育將沒有框架的限制<br>在這裡,你可以「學你想學、學你想成為」<br>在這裡,能夠客製化讓你創造一個獨一無二的自己。<br><br>現在,誠摯的邀請你們一起加入我們的建校大業,<br>成為雜學校第一批學生。</p>
+					
+					<img class="visible-xs" src="http://zashare.org/funding_assets/img/main/2-right.png" alt="">
+				
+				</div>
+				<div class="col-sm-1">
+					<p class="vertical_text">入學條件:1-99歲的每一個人<br>入學考試:破關『教育迷宮遊戲』<br>招生名額:2017位
+					</p>
+				</div>
+				<div class="col-sm-12">
+					<hr>
+				</div>
+				<div class="col-sm-8">
+					<h3>身為 <u>雜學校建校元老</u>,有什麼「特權」?</h3>
+					<div class="row">
+						<div class="col-sm-4 fcenter vertical">
+							<div class="num">01</div>
+							<h4>雜學校學生證</h4>	
+							<p>一張在手,雜學無窮!</p>
+						</div>
+						<div class="col-sm-4 fcenter vertical">
+							<div class="num">02</div>
+							<h4>雜博覽</h4>	
+							<p>2017年尊榮級觀展體驗<br>@華山1914</p>
+						</div>
+						<div class="col-sm-4 fcenter vertical">
+							<div class="num">03</div>
+							<h4>雜學堂</h4>	
+							<p>24 堂超狂講師<br>手把手教你最雜的知識</p>
+						</div>
+					</div>
+					<hr>
+					<h4>看更多建校內容>>> 
+						<a class="btn" href="http://zashare.org/pacman/" target="_blank">
+							立即支持建校
+						</a>
+					</h4>
+				</div>
+				<div class="col-sm-4">
+					<img class="hidden-xs" src="http://zashare.org/funding_assets/img/main/2-right.png" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="blue_bg">
 
 		<div class="container area_news">
