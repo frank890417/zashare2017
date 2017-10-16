7a4b5225e64e4bc60d3e0e69649e072fe201fea8
cheyuwu <frank890417@gmail.com>
2017-10-16 18:20:52 +0800

    updat event page version 2

--- resources/assets/js/pages/page_workshop_2017.vue
+++ resources/assets/js/pages/page_workshop_2017.vue
@@ -2,76 +2,100 @@
 .page_workshops
   .container
     .row
-      .col-sm-12
-        h1 工作坊活動
-        hr
-        p 工作坊報名資格：持有雜學校門票，在線上報名工作坊的同時，別忘了提前購買2017雜學校入場門票哦！
-        br
-      .col-sm-12.col-cata.fixed(v-if="fixed_cata_class")
-        .btn-group
-          .btn.btn-secondary(
-            v-for="time in Object.keys(time_chunk(workshop))",
-            @click="now_date = time",
-            :class="{'btn-primary': now_date == time}"
-          ) {{time}}
-      .col-sm-12.col-cata
-        .btn-group
-          .btn.btn-secondary(
-            v-for="time in Object.keys(time_chunk(workshop))",
-            @click="now_date = time",
-            :class="{'btn-primary': now_date == time}"
-          ) {{time}}
-        br
-        br
-  .container.container-table(v-for="ws in event_types")
-    .row
-      .col-sm-12
-        h2 {{ws.tag}}
-      .col-sm-12(
-        v-for="(events,time) in time_chunk(ws.data)",
-        v-if="now_date == time"
-        )
-        //h3 {{time}}
-        table.table.table-hover
-          thead
-            th.text-center(style="width: 10%") 時間
-            //th(style="width: 5%") 參展編號
-            //th(style="width: 10%") 攤位編號
-            th(style="width: 45%") 主題
-            th.hidden-xs(style="width: 15%") 單位
-            th.hidden-xs(style="width: 15%") 地點
-            //th(style="width: 25%") 描述
-            th.hidden-xs(style="width: 15%") 報名方式
-            //th(style="width: 5%") 報名連結
+      .col-sm-12.col-selector
+        .row
+          .col-sm-12
+            h1 活動與講座資訊
+            //hr
+            br
+            .col-sm-12.col-cata.fixed(v-if="fixed_cata_class")
+              .btn-group
+                .btn.btn-secondary(
+                  v-for="time in Object.keys(time_chunk(workshop))",
+                  @click="now_date = time",
+                  :class="{'btn-primary': now_date == time}"
+                ) {{time}}
+              .btn-group
+                .btn.btn-secondary(
+                  v-for="evt_type in event_types.map(o=>o.tag)",
+                  @click="now_event_type = evt_type",
+                  :class="{'btn-primary': now_event_type == evt_type}"
+                ) {{evt_type}}
+              br
+              br
 
-          tbody
-            tr(v-for="event in events")
-              td.text-center(v-html="replaceDash(event.time)")
-              //td {{event.tag_team}}
-              td
-                h3 {{event.title}}
-                p {{event.description}}
-                .visible-xs
-                  hr
-                  p.mobile_info
-                    | 單位：{{event.name}} <br>
-                    //({{event.tag}})
-                    | 地點：{{event.place}}<br>
-                    | 報名方式：{{event.register}}<br>
-                    .btn.btn-secondary.btn-primary(
-                      @click="registerEvent(event)"
-                      target="_blank",
-                      v-if="event.website"
-                    ) 報名
-              td.hidden-xs {{event.name}}
-                //<br>({{event.tag}})
-              td.hidden-xs {{event.place}}
-              td.hidden-xs {{event.register}}<br>
-                .btn.btn-secondary.btn-primary(
-                  @click="registerEvent(event)"
-                  target="_blank",
-                  v-if="event.website"
-                ) 報名
+              
+          .col-sm-12.col-cata
+            .btn-group
+              .btn.btn-secondary(
+                v-for="time in Object.keys(time_chunk(workshop))",
+                @click="now_date = time",
+                :class="{'btn-primary': now_date == time}"
+              ) {{time}}
+            br
+            br
+          .col-sm-12.col-cata
+            .btn-group
+              .btn.btn-secondary(
+                v-for="evt_type in event_types.map(o=>o.tag)",
+                @click="now_event_type = evt_type",
+                :class="{'btn-primary': now_event_type == evt_type}"
+              ) {{evt_type}}
+            br
+            br
+      .col-sm-12
+        .container-table(v-for="ws in event_types.filter(o=>o.tag==now_event_type)")
+          .row
+            .col-sm-12
+              h2 {{ws.tag}}
+              p(v-if="ws.tag=='雜工坊'") 工作坊報名資格：持有雜學校門票，在線上報名工作坊的同時，別忘了提前購買2017雜學校入場門票哦！
+              
+            .col-sm-12(
+              v-for="(events,time) in time_chunk(ws.data)",
+              v-if="now_date == time"
+              )
+              //h3 {{time}}
+              h4 共{{events.length}}場活動
+              table.table.table-hover
+                thead
+                  th.text-center(style="width: 10%") 時間
+                  //th(style="width: 5%") 參展編號
+                  //th(style="width: 10%") 攤位編號
+                  th(style="width: 45%") 主題
+                  th.hidden-xs(style="width: 15%") 單位
+                  th.hidden-xs(style="width: 15%") 地點
+                  //th(style="width: 25%") 描述
+                  th.hidden-xs(style="width: 15%") 報名方式
+                  //th(style="width: 5%") 報名連結
+
+                tbody
+                  tr(v-for="event in events")
+                    td.text-center(v-html="replaceDash(event.time)")
+                    //td {{event.tag_team}}
+                    td
+                      h3 {{event.title}}
+                      p {{event.description}}
+                      .visible-xs
+                        hr
+                        p.mobile_info
+                          | 單位：{{event.name}} <br>
+                          //({{event.tag}})
+                          | 地點：{{event.place}}<br>
+                          | 報名方式：{{event.register}}<br>
+                          .btn.btn-secondary.btn-primary(
+                            @click="registerEvent(event)"
+                            target="_blank",
+                            v-if="event.website"
+                          ) 報名
+                    td.hidden-xs {{event.name}}
+                      //<br>({{event.tag}})
+                    td.hidden-xs {{event.place}}
+                    td.hidden-xs {{event.register}}<br>
+                      .btn.btn-secondary.btn-primary(
+                        @click="registerEvent(event)"
+                        target="_blank",
+                        v-if="event.website"
+                      ) 報名
     //.row
       .col-sm-12
         pre(v-html="workshop_c")
@@ -114,7 +138,8 @@ export default {
           data: exforum.default
         }
       ],
-      now_date: "10/19(四)"
+      now_date: "10/19(四)",
+      now_event_type: "雜工坊"
     }
   },
   computed:{
@@ -127,6 +152,14 @@ export default {
       }
       
       return false
+    }
+  },
+  watch:{
+    now_date(){
+      $("html,body").animate({scrollTop:0})
+    },
+    now_type(){
+      $("html,body").animate({scrollTop:0})
     }
   },
   methods:{
