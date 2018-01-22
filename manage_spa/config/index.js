fd14a2da4f415c65d36f21a53a50803ddc196047
cheyuwu <frank890417@gmail.com>
2018-01-22 15:56:24 +0800

    udpate spa build

--- manage_spa/config/index.js
+++ manage_spa/config/index.js
@@ -52,7 +52,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     proxyTable: {
       '/api': {
         target: 'http://zashare.org/api/spa/',//设置你调用的接口域名和端口号 别忘了加http
