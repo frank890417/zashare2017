6eef7ded8e9cac4e0a83412e54a60b04931c52b4
cheyuwu <frank890417@gmail.com>
2018-04-23 13:11:58 +0800

    add soft scroll

--- frontend_spa/src/main.js
+++ frontend_spa/src/main.js
@@ -12,7 +12,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
+import softscroll from 'monoame-softscroll'
+softscroll.init()
 
 import Vue from 'vue'
 import App from './App'
