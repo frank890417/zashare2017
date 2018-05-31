f14e833641a17af699400cac25589ad10f0cde28
cheyuwu <frank890417@gmail.com>
2018-05-31 13:46:19 +0800

    update mail sender

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'zashare@ozzie-art.com'),
+        'name' => env('MAIL_FROM_NAME', 'ZASHARE 雜學校'),
     ],
 
     /*
